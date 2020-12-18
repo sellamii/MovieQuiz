c7ad0dcec81cc2d2eb6c7007d1c299f367acd337
zied <zied.sellami@lab5com.fr>
2020-12-18 18:17:57 +0100

    feat(header): add react-router,basic ui header

--- movieclient/src/App.js
+++ movieclient/src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
-
+import {BrowserRouter as Router , Route , Switch} from "react-router-dom"
+import LoginHeader from './Components/LoginHeader'
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <LoginHeader/>
+          </Route>
+        </Switch>
+      </Router>
     
     </div>
   );
---
+++ movieclient/src/Components/LoginHeader.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../Css/LoginHeader.css'
+import {Link} from 'react-router-dom'
+import movielogo from '../movielogo.png'
+import userlogo from '../zied.png'
+
+ function LoginHeader() {
+    
+
+    return (
+        <div className="Header">
+        <Link className="Link" to="/">
+             <img src={movielogo} alt="movielogo" className="HeaderLogo" />
+        </Link> 
+        <p className="Text">I LOVE MOVIES</p> 
+        <img src={userlogo} alt="userlogo" className="UserLogo"/>    
+         
+        </div>
+    )
+}
+
+export default LoginHeader ;
