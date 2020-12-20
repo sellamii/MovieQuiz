b5b319a8fab47c8b8e9ba25ad01be8cd3206137e
zied <zied.sellami@lab5com.fr>
2020-12-20 15:51:22 +0100

    feat(Main): implement Main page , fix css

--- movieclient/src/App.js
+++ movieclient/src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter as Router , Route , Switch} from "react-router-dom"
 import LoginHeader from './Components/LoginHeader'
 import Login from './Components/Login'
 import Register from './Components/Register'
-
+import Main from './Components/Main'
 function App() {
   return (
     <div className="App">
@@ -14,9 +14,13 @@ function App() {
             <Login />
           </Route>
           <Route path="/register">
-          <LoginHeader/>
-          <Register/>
-        </Route>
+            <LoginHeader/>
+            <Register/>
+          </Route>
+          <Route path="/">
+              <LoginHeader/>
+              <Main />
+              </Route>
         </Switch>
       </Router>
     
---
+++ movieclient/src/Components/Main.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../Css/Main.css'
+import Logo from '../logoquiz.png'
+
+ function Main(props) {
+    
+
+    return (
+       <div className="Main">
+       <div className="Main-body">
+       
+    
+             <div className="Main-body_logo"><img src={Logo} alt="LogoCineQuiz" height="180" width ="200"></img></div>
+             <div className="Main-body_rules">
+               <h2> Welcome to the quizz ! </h2>
+               <span className="pt5"> You'll be asked a series of <span className="text-bold">"Yes or No"</span> questions.</span>
+               <span className="pt5">Answer as many as you can in the allowed time ! </span>
+               <span className="pt5">Good luck !</span>
+             </div>
+             <button className="Main-body_button" >Let's Start</button>
+        
+       
+       </div>
+       </div>
+    )
+}
+
+export default Main ;
