8378e5e2d2ca57c199ef50135a0d016589ccd899
zied <zied.sellami@lab5com.fr>
2020-12-22 00:08:37 +0100

    feat(action): get types actions

--- movieclient/src/Actions/Action.js
+++ movieclient/src/Actions/Action.js
@@ -1,6 +1,13 @@
 
 import axios from 'axios'
 import * as CONSTANTS from '../Api/Const.js'
+import {GET_ALL_MOVIES_SUCCEED,GET_ALL_MOVIES_ERROR,GET_ALL_ACTORS_SUCCEED,
+        GET_ALL_ACTORS_ERROR,GET_DETAIL_ACTOR_SUCCEED,GET_DETAIL_ACTOR_ERROR,
+         GET_MOVIES_ACTOR_SUCCEED,GET_MOVIES_ACTOR_ERROR,GET_IMAGE_ACTOR_SUCCEED
+        ,GET_IMAGE_ACTOR_ERROR,GET_DETAIL_MOVIE_SUCCEED,GET_DETAIL_MOVIE_ERROR,
+         GET_IMAGE_MOVIE_SUCCEED,GET_IMAGE_MOVIE_ERROR
+        } from './Type'
+        
 const { API_BASE, API_KEY } = CONSTANTS
 
 
@@ -8,8 +15,8 @@ export const getMovies = (page) => {
     return dispatch => {
         return axios.get(`${API_BASE}/movie/popular?api_key=${API_KEY}&language=en-US${page}`)
         .then(response => response.data)
-        .then(movie => dispatch({type:"GET_ALL_MOVIES_COMPLETED", payload: [movie]}))
-        .catch((err) => dispatch({type:"GET_ALL_MOVIES_ERROR",payload:err}))
+        .then(movie => dispatch({type: GET_ALL_MOVIES_SUCCEED, payload: [movie]}))
+        .catch((err) => dispatch({type: GET_ALL_MOVIES_ERROR,payload:err}))
     }
 }
 
@@ -17,8 +24,8 @@ export const getActors = (page) => {
     return dispatch => {
         return axios.get(`${API_BASE}/person/popular?api_key=${API_KEY}&language=en-US&page=${page}`)
         .then(response => response.data)
-        .then(actor => dispatch({type:"GET_ALL_ACTORS_COMPLETED", payload: [actor]}))
-        .catch((err) => dispatch({type:"GET_ALL_ACTORS_ERROR",payload:err}))
+        .then(actor => dispatch({type: GET_ALL_ACTORS_SUCCEED, payload: [actor]}))
+        .catch((err) => dispatch({type: GET_ALL_ACTORS_ERROR,payload:err}))
     }
 }
 
@@ -26,8 +33,8 @@ export const getDetailActor = (actor_id) => {
     return dispatch => {
         return axios.get(`${API_BASE}/person/${actor_id}?api_key=${API_KEY}&language=en-US`)
         .then(response => response.data)
-        .then(actor => dispatch({type:"GET_DETAIL_ACTOR_COMPLETED", payload:[actor]}))
-        .catch((err) => dispatch({type:"GET_DETAIL_ACTOR_ERROR",payload:err}))
+        .then(actor => dispatch({type: GET_DETAIL_ACTOR_SUCCEED, payload:[actor]}))
+        .catch((err) => dispatch({type: GET_DETAIL_ACTOR_ERROR,payload:err}))
     }
 }
 
@@ -35,8 +42,8 @@ export const getMoviesOfActor = (actor_id) => {
     return dispatch => {
         return axios.get(`${API_BASE}/person/${actor_id}/movie_credits?api_key=${API_KEY}&language=en-US`)
         .then(response => response.data)
-        .then(actor => dispatch({type:"GET_MOVIES_ACTOR_COMPLETED", payload:[actor]}))
-        .catch((err) => dispatch({type:"GET_MOVIES_ACTOR_ERROR",payload:err}))
+        .then(actor => dispatch({type: GET_MOVIES_ACTOR_SUCCEED, payload:[actor]}))
+        .catch((err) => dispatch({type: GET_MOVIES_ACTOR_ERROR,payload:err}))
     }
 }
 
@@ -44,8 +51,8 @@ export const getImagesofActor = (actor_id) => {
     return dispatch => {
         return axios.get(`${API_BASE}/person/${actor_id}/images?api_key=${API_KEY}&language=en-US`)
         .then(response => response.data)
-        .then(actor => dispatch({type:"GET_IMAGE_ACTOR_COMPLETED", payload:[actor]}))
-        .catch((err) => dispatch({type:"GET_IMAGE_ACTOR_ERROR",payload:err}))
+        .then(actor => dispatch({type: GET_IMAGE_ACTOR_SUCCEED, payload:[actor]}))
+        .catch((err) => dispatch({type: GET_IMAGE_ACTOR_ERROR,payload:err}))
     }
 }
 
@@ -53,8 +60,8 @@ export const getDetailMovie = (movie_id) => {
     return dispatch => {
         return axios.get(`${API_BASE}/movie/${movie_id}?api_key=${API_KEY}&language=en-US`)
         .then(response =>  response.data)
-        .then(movie => dispatch({type:"GET_DETAIL_MOVIE_COMPLETED", payload:[movie]}))
-        .catch((err) => dispatch({type:"GET_DETAIL_MOVIE_ERROR",payload:err}))
+        .then(movie => dispatch({type: GET_DETAIL_MOVIE_SUCCEED, payload:[movie]}))
+        .catch((err) => dispatch({type: GET_DETAIL_MOVIE_ERROR,payload:err}))
     }
 }
 
@@ -62,8 +69,8 @@ export const getImagesofMovie = (movie_id) => {
     return dispatch => {
         return axios.get(`${API_BASE}/movie/${movie_id}/images?api_key=${API_KEY}&language=en-US&include_image_language=fr`)
         .then(response =>  response.data)
-        .then(movie => dispatch({type:"GET_IMAGE_MOVIE_COMPLETED", payload:[movie]}))
-        .catch((err) => dispatch({type:"GET_IMAGE_MOVIE_ERROR",payload:err}))
+        .then(movie => dispatch({type: GET_IMAGE_MOVIE_SUCCEED, payload:[movie]}))
+        .catch((err) => dispatch({type: GET_IMAGE_MOVIE_ERROR,payload:err}))
     }
 }
 
--- movieclient/src/Api/Const.js
+++ movieclient/src/Api/Const.js
@@ -1,3 +1,3 @@
 export const API_BASE = "https://api.themoviedb.org/3";
-export const API_KEY = "e0d3d49d19ce926b6842e10c9d97ba31";
+export const API_KEY = "7bcc43fda0a0d5751bd33404c1f0a34f";
 export const BASE_URL_IMAGE_ACTOR = "https://image.tmdb.org/t/p/w500";
